e45d878a257e8911635dae17bf81ecb626be35b7
Sreelakshmi Are <sreelakshmivec@gmail.com>
2020-05-09 19:16:13 +0530

    second commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Green India Trust (GIT)</title>
+    <title>Green India Trust</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('fonts/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('fonts/ionicons.min.css') }}">
