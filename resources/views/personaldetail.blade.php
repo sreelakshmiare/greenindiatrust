bd5119a7a0a8fcaf10f646a3f57bd0a317c2a601
Sreelakshmi Are <sreelakshmivec@gmail.com>
2020-05-20 13:11:02 +0530

    three changes

--- resources/views/personaldetail.blade.php
+++ resources/views/personaldetail.blade.php
@@ -1,59 +1,78 @@
 @extends('layouts.index')
 @section('center')
 
-<div class="container">
-    <h5 class="text--justify text-success">Personal Information</h5>
-</div>
 <section class="section-about">
-    <div class="panel">
-        <div class="container">
-            <form>
+    <div class="container">
+        <h5 class="text--justify text-success">Personal Information</h5>
+        <div class="class-form">
+            
+            <form class="bg-light shadow form">
+                
                 <div class="form-row">
-                    <div class="col-lg-6 form-group"><input class="form-control" type="text" placeholder="Firstname"></div>
-                    <div class="col-lg-6 form-group"><input class="form-control" type="text" placeholder="Lastname"></div>
+                    <div class="col-lg-5 offset-lg-1 form-group"><input type="text" class="form-control" placeholder="Firstname" />
+                    </div>
+                    <div class="col-lg-5 form-group"><input type="text" class="form-control" placeholder="Lastname" />
+                    </div>
                 </div>
-            </form>
-        </div>
-        <div class="container">
-            <form>
+               
                 <div class="form-row">
-                    <div class="col-lg-6 form-group"><input class="form-control" type="email" placeholder="Email"></div>
-                    <div class="col-lg-6 form-group"><input class="form-control" type="password" placeholder="Password"></div>
+                    <div class="col-lg-5 offset-lg-1 form-group"><input type="email" class="form-control" placeholder="Email" />
+                    </div>
+                    <div class="col-lg-5 form-group"><input type="password" class="form-control" placeholder="Password" />
+                    </div>
                 </div>
-            </form>
-        </div>
-        <div class="container">
-            <form>
+                
                 <div class="form-row">
-                    <div class="col">
-                        <div class="form-group"><input class="form-control" type="text" placeholder="Address"></div>
+                    <div class="col-lg-10 offset-lg-1 form-group">
+                        <textarea class="form-control" placeholder="Address" rows="5">
+                        </textarea>
+                    </div>
+                </div>
+                
+                <div class="form-row">
+                    <div class="col-lg-5 offset-lg-1 form-group">
+                            <input type="text" class="form-control" placeholder="City" />
+                    </div>
+                    <div class="col-lg-3 form-group">
+                            <select class="form-control">
+                                <option value="12" selected>Choose State</option>
+                                <option value="13">Andhra Pradesh</option><option value="14">Arunachal Pradesh</option>
+                                <option value>Assam</option><option value>Bihar</option><option value>Chandigarh</option>
+                                <option value>Chhattisgarh</option><option value>Delhi</option><option value>Goa</option>
+                                <option value>Gujarat</option><option value>Haryana</option><option value>Himachal Pradesh</option>
+                                <option value>Jharkhand</option><option value>Karnataka</option><option value>Kerala</option>
+                                <option value>Madhya Pradesh</option><option value>Maharashtra</option>
+                                <option value>Manipur</option><option value>Meghalaya</option>
+                                <option value>Mizoram</option><option value>Nagaland</option>
+                                <option value>Odisha</option><option value>Punjab</option>
+                                <option value>Rajasthan</option><option value>Sikkim</option>
+                                <option value>Tamil Nadu</option><option value>Telangana</option>
+                                <option value>Tripura</option><option value>Uttar Pradesh</option>
+                                <option value>Uttarakhand</option><option value>West Bengal</option>
+                            </select>
+                    </div>
+                    <div
+                        class="col-lg-2 form-group"><input type="text" class="form-control" placeholder="Zip" />
+                    </div>
+                </div>
+       
+                <div class="form-row">
+                    <div class="col-lg-5 offset-lg-1 form-group">
+                        <input type="text" class="form-control" placeholder="Aadhar No." />
+                    </div>
+                    <div class="col-lg-3 form-group">
+                        <input type="text" class="form-control" placeholder="PAN" />
+                    </div>
+                </div>
+       
+                <div class="form-row">
+                    <div class="col-lg-2 offset-lg-1 text-left form-group">
+                            <a class="btn btn-success" role="button" href="razorpay.html">Sign Up</a>
                     </div>
                 </div>
             </form>
         </div>
-        <div class="container">
-            <form>
-                <div class="form-row">
-                    <div class="col-lg-6 form-group"><input class="form-control" type="text" placeholder="City"></div>
-                    <div class="col-lg-4 form-group"><select class="form-control"><optgroup label="This is a group"><option value="12" selected="">Choose State</option><option value="13">This is item 2</option><option value="14">This is item 3</option></optgroup></select></div>
-                    <div
-                        class="col-lg-2 form-group"><input class="form-control" type="text" placeholder="Zip"></div>
-        </div>
-        </form>
-    </div>
-    <div class="container">
-        <form>
-            <div class="form-row">
-                <div class="col-lg-6 form-group"><input class="form-control" type="number" placeholder="Aadhar No."></div>
-                <div class="col-lg-4 form-group"><input class="form-control" type="number" placeholder="PAN"></div>
-            </div>
-        </form>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col"><a class="btn btn-success" role="button" href="razorpay.html">Sign in</a></div>
-        </div>
-    </div>
     </div>
 </section>
+
 @endsection
--- resources/views/smokefree.blade.php
+++ resources/views/smokefree.blade.php
@@ -1,21 +1,114 @@
 @extends('layouts.index')
 @section('center')
 <section class = "section-about">
+<div class="container text-center">
+        <div class="row">
+            <div class="col-md-12">
+                
+                    <h4 class="text-justify text-success">Smoke Free:The Effects of Smoking on the Body</h4>
+                    <p class="text-justify text-success">No matter how you smoke it, tobacco is dangerous to your health. There are no safe substances in any tobacco products, from acetone and tar to nicotine and carbon monoxide. The substances you inhale dont just affect your lungs. They can affect your entire body. 
+					</p>
+
+					<p class="text-justify text-success">Smoking can lead to a variety of ongoing complications in the body, as well as long-term effects on your body systems. While smoking can increase your risk of a variety of problems over several years, some of the bodily effects are immediate. Learn more about the symptoms and overall Tobacco smoke is incredibly harmful to your health. There is no safe way to smoke. Replacing your cigarette with a cigar, pipe, or hookah won't help you avoid the health risks.effects of smoking on the body below.
+					</p>
+
+					<p class="text-justify text-success">Tobacco smoke is incredibly harmful to your health. There's no safe way to smoke. Replacing your cigarette with a cigar, pipe, or hookah won't help you avoid the health risks. 
+					</p>
+					 
+					
+
+					 <p class="text-justify text-success">Cigarettes contain about 600 ingredients, many of which can also be found in cigars and hookahs. When these ingredients burn, they generate more than 7,000 chemicals, according to the American Lung Association. Many of those chemicals are poisonous and at least 69 of them are linked to cancer.
+					 </p>
+
+					 <p class="text-justify text-success">In the United States, the mortality rate for smokers is three times that of people who never smoked. In fact, the Centers for Disease Control and Prevention (CDC) say that smoking is the most common "preventable cause of death" in the United States. While the effects of smoking may not be immediate, the complications and damage can last for years. The good news is that quitting smoking can reverse many effects.
+					 </p>
+					
+					 <h4 class="text-justify text-success">Central Nervous System:</h4>
+					 	
+						 <p class="text-justify text-success">One of the ingredients in tobacco is a mood-altering drug called nicotine. <b>Nicotine </b>reaches your brain in mere seconds and makes you feel more energized for a while. But as that effect wears off, you feel tired and crave more. Nicotine is extremely habit-forming, which is why people find smoking <b>so difficult to quit</b>. Physical withdrawal from nicotine can impair your cognitive functioning and make you feel anxious, irritated, and depressed. Withdrawal can also cause headaches and sleep problems.
+						 </p>
+							
+							 <h4 class="text-justify text-success">Respiratory System:</h4>
+							 <p class="text-justify text-success">When you inhale smoke, you are taking in substances that can damage your lungs. Over time, this damage leads to a variety of problems. Along with increased infections, people who smoke are at higher risk for chronic nonreversible lung conditions such as:
+							</p>
+            </div>
+            
+        </div>
+    </div>
+	
+</section>
+
+<section class="section-about">
+        <div class="container">
+            <div class="row">
+<div class = "col-md-12">
+		 
+			<ol class = "orderlist text-success">
+			
+                <li class="text-success"><u><b>emphysema,</b></u> the destruction of the air sacs in your lungs.</li>
+                <li class="text-success"><u><b>chronic bronchitis, </b></u>permanent inflammation that affects the lining of the breathing tubes of the lungs. </li>
+				<li class="text-success"><b><u>chronic obstructive pulmonary disease (COPD), a group of lung diseases</b></u></li>
+                <li class="text-success"><b><u>Lung cancer.</u></b></li>
+				
+			</ol>
+		
+		 <p class="text-justify text-success">Withdrawal from tobacco products can cause temporary congestion and respiratory discomfort as your lungs and airways begin to heal. Increased mucus production right after quitting smoking is a positive sign that your respiratory system is recovering. Children whose parents smoke are more prone to coughing, wheezing, and asthma attacks than children whose parents don't. They also tend to have higher rates of <b><u>pneumonia and bronchitis. </u></b>
+		 </p>
+		
+		<h4 class="text-justify text-success">Cardiovascular System:</h4>
+		  <p class="text-justify text-success">Smoking damages your entire cardiovascular system. Nicotine causes blood vessels to tighten, which restricts the flow of blood. Over time, the ongoing narrowing, along with damage to the blood vessels, can cause disease. Smoking also raises blood pressure, weakens blood vessel walls, and increases <b>blood clots</b>. Together, this raises your risk of <b>stroke</b>.
+			You are also at an increased risk of worsening heart disease if you have already had <b>heart bypass surgery, a heart attack</b>, or a <b>stent</b> placed in a blood vessel.<br/>
+			Smoking not only impacts your cardiovascular health, but also the health of those around you who don't smoke. Exposure to secondhand smoke carries the same risk to a nonsmoker as someone who does smoke. Risks include stroke, heart attack, and <b>heart disease</b>. 
+		</p>
+
+		
+</div>
+</div>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col offset-lg-3"><img class="img-thumbnail" src="{{ assets('img/smokefree.jpg') }} " width="500" height="650"></div>
+        </div>
+    </div><section class = "section-about">
     <div class="container text-center">
         <div class="row">
             <div class="col-md-12">
                 
-                    <h4 class="text-justify text-success">Smoke Free</h4>
-                    <p class="text-justify text-success"> <strong>Upcoming Project......</strong> 
+                    <h4 class="text-justify text-success">Integumentary System (Skin,Hair,Nails)</h4>
+                    <p class="text-justify text-success">The more obvious signs of smoking involve skin changes. Substances in tobacco smoke actually change the structure of your skin. A recent study has shown that smoking dramatically increases the risk of squamous cell carcinoma (skin cancer).
+					</p>	
+					
+					<p class="text-justify text-success">Your fingernails and toenails aren’t immune from the effects of smoking. Smoking increases the likelihood of fungal nail infections. Hair is also affected by nicotine. An older study found it increases hair loss, balding, and graying. 
 					</p>
-					<br/><br/<br/><br/><br/>
-					<p class="text-justify text-success">
+					
+					<h4 class="text-justify text-success">Digestive System:</h4>
+					<p class="text-justify text-success">Smoking increases the risk of mouth, throat, larynx, and esophagus cancer. Smokers also have higher rates of pancreatic cancer. Even people who “smoke but don’t inhale” face an increased risk of mouth cancer.
+					Smoking also has an effect on insulin, making it more likely that you’ll develop insulin resistance. That puts you at increased risk of type 2 diabetes and its complications, which tend to develop at a faster rate than in people who don’t smoke.
 					</p>
-					<br/><br/>
-					<br/><br/>
-						
-				
-               
+
+					<h4 class="text-justify text-success">Sexuality and Reproductive System:</h4>
+					<p class="text-justify text-success">Nicotine affects blood flow to the genital areas of both men and women. For men, this can decrease sexual performance. For women, this can result in sexual dissatisfaction by decreasing lubrication and the ability to reach orgasm. Smoking may also lower sex hormone levels in both men and women. This can possibly lead to decreased sexual desire. 
+					</p>
+
+					<h4 class="text-justify text-success">Explore quit methods:</h4>
+					<p class="text-justify text-success">Quitting smoking is difficult, but your doctor can help you make a plan. Ask them for advice. There are a variety of nonprescription and prescription medications that can help you quit. You can also turn to our smoking cessation resource center, which has advice, stories from others, and more. There are both short and long-term benefits to quitting smoking. Since smoking affects every body system, finding a way to quit is the most important step you can take to living a longer and happier life. Some quit smoking methods have been shown to be more effective than others.
+					</p>
+					
+					<h4 class="text-justify text-success">Self-help Guides and other Materials:	</h4>
+						<ol class="orderlist text-justify text-success">
+							<li>Includes pamphlets, booklets, manuals, and videos, audio.</li>
+							<li>Many guides and other materials are available but vary in quality.</li>
+							<li>You can start counseling to help you get ready to quit and then continue counseling to help you stay quit.</li>
+							<li>State of deep concentration in which you are guided through some thoughts and feelings.</li>
+							<li>Aims to help people quit smoking by reducing the desire to smoke, increasing the will to quit, or helping to focus on quitting programs.</li>
+							<li>Needles are placed in certain parts of the body, including outside of the ear.</li>
+							<li>Aims to relieve discomfort from nicotine withdrawal.</li>
+							<li>Combining certain medications can be more effective than either medication by itself.</li>
+						</ol>
+
+					
+					
             </div>
             
         </div>
